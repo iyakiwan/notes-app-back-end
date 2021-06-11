e1cc4a1c89388e1b52e7a6e3f478f6818741ae92
Mufti Alie S <iyakiwan19@gmail.com>
2021-06-11 09:17:50 +0700

    Fix private url

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.89.97',
     routes: {
       cors: {
         origin: ['*'],
