029f248f75d624406cf89703684aeaae83b7641c
Mufti Alie S <iyakiwan19@gmail.com>
2021-06-11 14:07:06 +0700

    data validation implemented

--- src/api/notes/index.js
+++ src/api/notes/index.js
@@ -2,10 +2,10 @@ const NotesHandler = require('./handler');
 const routes = require('./routes');
 
 module.exports = {
-    name: 'notes',
-    version: '1.0.0',
-    register: async (server, { service }) => {
-        const notesHandler = new NotesHandler(service);
-        server.route(routes(notesHandler));
-    },
+  name: 'notes',
+  version: '1.0.0',
+  register: async (server, { service, validator }) => {
+    const notesHandler = new NotesHandler(service, validator);
+    server.route(routes(notesHandler));
+  },
 };
