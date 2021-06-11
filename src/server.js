be26d3dd4955f6597db375c365a625eed7aa2918
Mufti Alie S <iyakiwan19@gmail.com>
2021-06-11 09:08:05 +0700

    fix url serve

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '52.23.233.48',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
